bc49bf5e34364d850d86b3814934bdd6764ecc88
Andrea Lagaccia <a.lagaccia@gmail.com>
2022-01-30 23:10:34 +0100

    Description in config file

--- config/sendinblue.php
+++ config/sendinblue.php
@@ -1,6 +1,17 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Sendinblue variables
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the list to use for syncing contacts
+    | and the api key to connect to the api server of Sendinblue
+    |
+    */
+
     'API_KEY' => env('SENDINBLUE_API_KEY'),
     'LIST_ID' => env('SENDINBLUE_LIST_ID'),
 ];
