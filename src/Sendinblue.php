bc0a5164864571e540ce142c235874273953278d
Andrea Lagaccia <a.lagaccia@gmail.com>
2022-01-31 00:18:04 +0100

    change methods' name

--- src/Sendinblue.php
+++ src/Sendinblue.php
@@ -12,28 +12,28 @@ class Sendinblue
 
     public function __construct()
     {
-        $this->api_key();
-        $this->api_base_url();
-        $this->list_id();
-        $this->setHeaders();
+        $this->set_api_key();
+        $this->set_api_base_url();
+        $this->set_list_id();
+        $this->set_headers();
     }
 
-    public function api_key()
+    public function set_api_key()
     {
         $this->api_key = config('sendinblue.API_KEY') ?? env('SENDINBLUE_API_KEY');
     }
 
-    public function api_base_url()
+    public function set_api_base_url()
     {
         $this->api_base_url = self::API_BASE_URL;
     }
 
-    public function list_id()
+    public function set_list_id()
     {
         $this->list_id = config('sendinblue.LIST_ID') ?? env('SENDINBLUE_LIST_ID');
     }
 
-    public function setHeaders()
+    public function set_headers()
     {
         $this->headers = [
             'Accept' => 'application/json',
