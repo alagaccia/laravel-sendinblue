5946203819ecf1cdf388b3f1f309eed07bef815d
Andrea Lagaccia <a.lagaccia@gmail.com>
2022-01-30 23:57:03 +0100

    Contact methods

--- src/Contact.php
+++ src/Contact.php
@@ -5,20 +5,55 @@ use AndreaLagaccia\Sendinblue\Sendinblue;
 
 class Contact extends Sendinblue
 {
-    public function create($client, $list_id = null)
+    protected $url;
+
+    public function __construct()
     {
-        \Http::withHeaders([
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-                'Api-Key' => config('sendinblue.API_KEY'),
-            ])->post('https://api.sendinblue.com/v3/contacts', [
+        $this->url = $this->API_BASE_URL . 'contacts/';
+    }
+
+    public function create($email, $ATTRIBUTES, $list_id = null)
+    {
+        $method_url = $this->url;
+
+        $res = \Http::withHeaders($this->headers)->post($method_url, [
                 'updateEnabled' => false,
-                'listIds' => [2],
-                'email' => $client->email,
-                'attributes' => [
-                    'SEX' => $client->sex ?? null,
-                ]
+                'listIds' => [$list_id ?? $this->getListId()],
+                'email' => $email,
+                'attributes' => $ATTRIBUTES,
             ]);
+
+        return $res;
+    }
+
+    public function update($email, $ATTRIBUTES, $list_id = null)
+    {
+        $method_url = $this->url . urlencode($email);
+
+        $res = \Http::withHeaders($this->headers)->put($method_url, [
+                'listIds' => [$list_id ?? $this->getListId()],
+                'attributes' => $ATTRIBUTES,
+            ]);
+
+        return $res;
+    }
+
+    public function delete($email)
+    {
+        $method_url = $this->url . urlencode($email);
+
+        $res = \Http::withHeaders($this->headers)->delete($method_url);
+
+        return $res;
+    }
+
+    public function info($email)
+    {
+        $method_url = $this->url . urlencode($email);
+
+        $res = \Http::withHeaders($this->headers)->get($method_url);
+
+        return $res;
     }
 
 }
--- src/Sendinblue.php
+++ src/Sendinblue.php
@@ -5,13 +5,29 @@ class Sendinblue
 {
     protected $api_key;
     protected $list_id;
+    protected $headers;
 
-    const API_URL = "https://api.sendinblue.com/v3/";
+    protected const API_BASE_URL = "https://api.sendinblue.com/v3/";
 
     public function __construct()
     {
         $this->api_key = config('sendinblue.API_KEY') ?? env('SENDINBLUE_API_KEY');
         $this->list_id = config('sendinblue.LIST_ID') ?? env('SENDINBLUE_LIST_ID');
+        $this->headers = $this->setHeaders();
+    }
+
+    public function setHeaders()
+    {
+        return [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+                'Api-Key' => config('sendinblue.API_KEY'),
+            ];
+    }
+
+    public function getListId()
+    {
+        return $this->list_id;
     }
 
 }
