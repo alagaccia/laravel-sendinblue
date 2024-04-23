a5793cc33e6a18eeb734882dfff3f8a769c7e40f
Andrea Lagaccia <info@deltadigital.it>
2024-04-23 12:55:41 +0200

    TransactionalEmail

---
+++ src/TransactionalEmail.php
@@ -0,0 +1,41 @@
+<?php
+namespace AndreaLagaccia\Sendinblue;
+
+use AndreaLagaccia\Sendinblue\Sendinblue;
+use Illuminate\Support\Facades\DB;
+
+class TransactionalEmail extends Sendinblue
+{
+    protected $url;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->url = $this->api_base_url . 'smtp/email';
+    }
+
+    public function send($number, $content)
+    {
+        $method_url = $this->url;
+
+        $res = \Http::withHeaders($this->api_headers)->post($method_url, [
+                'type' => 'transactional',
+                'unicodeEnabled' => false,
+                'sender' => $this->sms_sender_name,
+                'recipient' => str_replace(' ', '', $number),
+                'content' => $content,
+                'webUrl' => $this->sms_webhook,
+            ]);
+
+        if ( $this->setting_sms_counter_column_name ) {
+            DB::table($this->setting_table_name)
+                ->where("{$this->setting_column_name}", "{$this->setting_sms_counter_column_name}")
+                ->update([
+                    "{$this->setting_sms_counter_value_name}" => $res->remaining_credit,
+                ]);
+        }
+
+        return $res->object();
+    }
+}
--- src/TransactionalSms.php
+++ src/TransactionalSms.php
@@ -12,29 +12,24 @@ class TransactionalSms extends Sendinblue
     {
         parent::__construct();
 
-        $this->url = $this->api_base_url . 'transactionalSMS/sms/';
+        $this->url = $this->api_base_url . 'transactionalSMS/sms';
     }
 
-    public function send($number, $content)
+    public function send(array $to, int $templateId, array $params, array $tags = null)
     {
         $method_url = $this->url;
 
         $res = \Http::withHeaders($this->api_headers)->post($method_url, [
-                'type' => 'transactional',
-                'unicodeEnabled' => false,
-                'sender' => $this->sms_sender_name,
-                'recipient' => str_replace(' ', '', $number),
-                'content' => $content,
-                'webUrl' => $this->sms_webhook,
+                'to' => [
+                    [
+                        'email' => $to['email'],
+                        'name' => $to['name'],
+                    ]
+                ],
+                'templateId' => $templateId,
+                'params' => $params,
+                'tags' => $tags,
             ]);
-
-        if ( $this->setting_sms_counter_column_name ) {
-            DB::table($this->setting_table_name)
-                ->where("{$this->setting_column_name}", "{$this->setting_sms_counter_column_name}")
-                ->update([
-                    "{$this->setting_sms_counter_value_name}" => $res->remaining_credit,
-                ]);
-        }
 
         return $res->object();
     }
