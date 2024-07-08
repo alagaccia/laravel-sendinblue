f3e09cd16afe997752c1f5001627a52eeda3a550
Andrea Lagaccia <info@deltadigital.it>
2024-07-08 17:11:57 +0200

    getTemplateInformation

--- src/TransactionalEmail.php
+++ src/TransactionalEmail.php
@@ -7,12 +7,14 @@ use Illuminate\Support\Facades\DB;
 class TransactionalEmail extends Sendinblue
 {
     protected $url;
+    protected $template_url;
 
     public function __construct()
     {
         parent::__construct();
 
         $this->url = $this->api_base_url . 'smtp/email';
+        $this->template_url = $this->api_base_url . 'smtp/templates';
     }
 
     public function send(array $to, int $templateId = null, string $htmlContent = null, array $params, array $tags = null)
@@ -32,6 +34,13 @@ class TransactionalEmail extends Sendinblue
                 'params' => $params,
                 'tags' => $tags,
             ]);
+
+        return $res->object();
+    }
+
+    public function getTemplateInformation($template_id)
+    {
+        $res = \Http::withHeaders($this->api_headers)->get("{$this->template_url}/{$template_id}");
 
         return $res->object();
     }
